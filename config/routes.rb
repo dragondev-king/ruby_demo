5cc0967aab6ce1c0278c8e2a31b31e9b836911d5
Kenji Nakamura <dragondev1017@gmail.com>
2022-02-03 11:23:20 +0900

    first commit - Get articles and Retrieve article

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  get "articles/", to: "articles#index"
+  get "articles/:id", to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles
 end
